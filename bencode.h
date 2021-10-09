3f52a5e4cd78de26266bc804c1f59f220d11381e
Julian Cahill <cahill.julian@gmail.com>
2021-10-10 10:30:06 +1300

    added license. added comments to bencode struct.

--- bencode.h
+++ bencode.h
@@ -1,3 +1,23 @@
+/*
+	Copyright (c) 2021 Julian Cahill <cahill.julian@gmail.com>
+
+	This software is provided 'as-is', without any express or implied
+	warranty. In no event will the authors be held liable for any damages
+	arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any purpose,
+	including commercial applications, and to alter it and redistribute it
+	freely, subject to the following restrictions:
+
+	1. The origin of this software must not be misrepresented; you must not
+	   claim that you wrote the original software. If you use this software
+	   in a product, an acknowledgment in the product documentation would be
+	   appreciated but is not required.
+	2. Altered source versions must be plainly marked as such, and must not be
+	   misrepresented as being the original software.
+	3. This notice may not be removed or altered from any source distribution.
+*/
+
 #ifndef BENCODE_H
 #define BENCODE_H
 
@@ -16,20 +36,22 @@ struct bencode {
 		BENCODE_INT = 1, BENCODE_BYTES,
 		BENCODE_LIST, BENCODE_DICT
 	} type;
+	
 	union {
-		int64_t i;
-		
-		char *bytes;
-		
-		struct bencode *list;
-		struct bencode *dict;
+		int64_t i;            // BENCODE_INT
+		char *bytes;          // BENCODE_BYTES
+		struct bencode *list; // BENCODE_LIST
+		struct bencode *dict; // BENCODE_DICT
 	};
 	
-	size_t length; // BENCODE_BYTES
+	char *key; // BENCODE_DICT
+	
+	union {
+		size_t length;     // BENCODE_BYTES
+		size_t key_length; // BENCODE_DICT
+	};
 	
 	struct bencode *next; // BENCODE_DICT | BENCODE_LIST
-	char *key; // BENCODE_DICT
-	size_t key_length; // BENCODE_DICT
 };
 
 char* bencode_parse(char *str, struct bencode *dest);
